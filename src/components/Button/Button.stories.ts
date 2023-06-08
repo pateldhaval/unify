049cb2afcb11d268ef97df6e4f3dd0dafb5d2788
Dhaval Patel <pateldhaval3890@gmail.com>
2023-06-08 09:40:53 +0530

    button stories added

--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -15,8 +15,44 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
 
+export const Large: Story = {
+	args: {
+		size: 'lg'
+	}
+};
+
+export const Small: Story = {
+	args: {
+		size: 'sm'
+	}
+};
+
+export const Secondary: Story = {
+	args: {
+		variant: 'secondary'
+	}
+};
+
+export const Destructive: Story = {
+	args: {
+		variant: 'destructive'
+	}
+};
+
 export const Outline: Story = {
 	args: {
 		variant: 'outline'
+	}
+};
+
+export const Ghost: Story = {
+	args: {
+		variant: 'ghost'
+	}
+};
+
+export const Link: Story = {
+	args: {
+		variant: 'link'
 	}
 };
